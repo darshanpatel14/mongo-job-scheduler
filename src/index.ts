b8edccf54dd4d8ad8adefe2c79dbfe6ca096b2b1
darshanpatel14 <57483591+darshanpatel14@users.noreply.github.com>
2026-01-03 15:16:29 +0530

    refactor: Replace broad barrel exports with specific named exports for core components and granular types.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
-export * from "./types";
-export * from "./events";
-export * from "./store";
-export * from "./worker";
-export * from "./core/scheduler";
+export { Scheduler } from "./core/scheduler";
+export { MongoJobStore } from "./store/mongo/mongo-job-store";
+export { InMemoryJobStore } from "./store/in-memory-job-store";
+
+export * from "./types/job";
+export * from "./types/retry";
+export * from "./types/repeat";
